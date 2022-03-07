0333a8bc4a5900bf74021685ace3af4fac379227
sirgama <sirengogamaliel@gmail.com>
2022-03-07 12:21:03 +0300

    style: remove commented out code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 function output(){
 
     let userPerson = document.getElementById("usrname").value;
-    let day = document.getElementById("day").value;
-    let month = document.getElementById("month").value;
+    let day = parseInt(document.getElementById("day").value);
+    let month = parseInt(document.getElementById("month").value) ;
     let year = document.getElementById("year").value;
     let userBirthday = month+"-"+ day + "-" + year;
     let male = document.getElementById("male");
     let female = document.getElementById("female");
-    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    maleGhanian = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    femaleGhanian = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    let weekDays = ["Monday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
+    maleGhanian = ['Kwadwo','Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame','Kwasi', ];
+    femaleGhanian = ['Adwoa','Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama','Akosua'];
     
     
     
@@ -42,17 +42,17 @@ function output(){
         else if (parseInt(datePop)<=0 && parseInt(datePop)>31){
             alert("Enter a date between 1 & 31");
         }
-        else if (parseInt(monthPop)<=0 && parseInt(monthPop)>12){
+        else if (parseInt(monthPop)<=0 && parseInt(monthPop)>15){
             alert("Enter month between 1 and 12");
         }
         else {
-            let dayOfWeek = ( ( (parseInt(cc)/4) -2*parseInt(cc)-1) + ((5* parseInt(yy)/4) ) + ((26*(parseInt(month)+1)/10)) +parseInt(day) ) % 7;
+            let dayOfWeek = ( ( (cc/4) -2*cc-1) + ((5* yy/4) ) + ((26*(month+1)/10)) +day ) % 7;
             let index = weekDays.indexOf(weekDays[parseInt(dayOfWeek)]);
-            console.log(index);
+            console.log(index, datee, datePop, monthPop, yearPop);
 
 
             console.log(parseInt(dayOfWeek));
-            /* let dayValue = dayOfTheWeek(datePop, (monthPop + 1), yearPop); */
+            
             if (male.checked === true){
                 document.getElementById("nametitle").innerHTML = `Hi, ${userPerson}`; `if I got your name right`;
                 
@@ -81,26 +81,4 @@ function output(){
 function reset(){
     document.getElementById('akandata').reset();
 }
-/* function dayOfTheWeek(day, month, year){
-    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    maleGhanian = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    femaleGhanian = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-    
-    
-    console.log(dayOfWeek) */
-    /* let dayOfWeek = (day + parseInt(((month + 1) * 26) / 10) + year + parseInt(year / 4) + 6 * parseInt(year / 100) + parseInt(year / 400) - 1) % 7; */
-    /* if (male.checked === true) {
-      return [maleGhanian[dayOfWeek] , weekDays[dayOfWeek]];
-        
-    }
-    else if (female.checked === true ){
-      return [femaleGhanian[dayOfWeek] , weekDays[dayOfWeek]];
-        
-    }
-    else {
-        alert("Kindly select your gender");
-    }
-    
-}
- */
 
